5d26d19b496cc7c207002d9c1965691bc4c66fee
JasonDesktop <MaJason93@gmail.com>
2022-07-28 13:52:59 -0700

    Comments on API

--- src/API/mockData.api.js
+++ src/API/mockData.api.js
@@ -1,5 +1,7 @@
 export async function mockData() {
     // Data is generated through MockAPi.com//
+    // Interesting thing to note, MockAPI uses Faker.js to generate unique values for each key.
+    // I initially thought that each month would have multiple customers, 
 
    const mockRequest = await fetch('https://62e2c909b54fc209b8807084.mockapi.io/api/v1/Sales')
     .then((response) => response.json())
